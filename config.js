dc70269850c46022fa75e2a6424badb27606da10
redzstormid <redzstorm@gmail.com>
2025-05-11 23:10:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzoUWWqgwM-IIsVlPcdCfr2AkXUJRkz89XuoywE2m3wEcMXLawojQ3nVdSyQsRkQMOh/exec';
+const LIFF_ID_QR = '2007226288-k77xbmX7';
+const LIFF_ID_SCAN = '2007226288-Qj0NDKY0';
+const LIFF_ID_SUPERADMIN = '2007226288-bNnjgPkn';
+const LIFF_ID_ADMIN = '2007226288-r5X4aV5X';
