d1ff60fc7cc707fb5ef40ebac40d90be5c4b1b32
redzstormid <redzstorm@gmail.com>
2025-05-13 21:40:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxGRDdkWU5O4q7CI60CbrMd7h1ufw96S0HgQBU5LoWDc4mGfVSkKrzsnP91zy1LRVKXVg/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxQBIJekSn-EJ7GfURGiPvEgsLLK_FMene3TDXATb9N5BYIUUQnvcc-_IdIPFwgyryZXg/exec';
 const LIFF_ID_QR = '2007226288-k77xbmX7';
 const LIFF_ID_SCAN = '2007226288-Qj0NDKY0';
 const LIFF_ID_SUPERADMIN = '2007226288-bNnjgPkn';
